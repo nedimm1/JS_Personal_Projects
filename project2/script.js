3a1dc8521138906e4de7432d7b7bad2f86dfb0b2
github_username <nedimmuminovic61@gmail.com>
2023-09-07 14:37:57 +0200

    placing the mark

--- project2/script.js
+++ project2/script.js
@@ -1,8 +1,14 @@
 'use strict';
 const boardButtons = document.querySelectorAll('.cell');
+const markElement = document.querySelector('.mark')
+
+function addMark(mark){
+  mark.textContent = "X"
+}
 
 function buttonClickEvent(){
     console.log('X')
+    addMark(markElement)
 }
 
 boardButtons.forEach(function(button){
