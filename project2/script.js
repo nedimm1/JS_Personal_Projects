bd389e33d197d1fbac4f775a0222d564d1262557
github_username <nedimmuminovic61@gmail.com>
2023-09-12 19:35:53 +0200

    switching between X and O

--- project2/script.js
+++ project2/script.js
@@ -45,5 +45,254 @@ boardButton1.addEventListener('click', function(){
     }
 })
 
+boardButton2.addEventListener('click', function(){
+    console.log(i)
+    if(i == 1){
+        boardButton2.textContent = 'X'
+        i++
+    }else if(i == 2){
+        boardButton2.textContent = 'O'
+        i++
+    }else if(i == 3){
+        boardButton2.textContent = 'X'
+        i++
+    }else if(i == 4){
+        boardButton2.textContent = "O"
+        i++
+    }else if(i == 5){
+        boardButton2.textContent = "X"
+        i++
+    }else if(i == 6){
+        boardButton2.textContent = "O"
+        i++
+    }else if(i == 7){
+        boardButton2.textContent = "X"
+        i++
+    }else if(i == 8){
+        boardButton2.textContent = "O"
+        i++
+    }else if(i == 9){
+        boardButton2.textContent = 'X'
+    }
+})
+
+boardButton3.addEventListener('click', function(){
+    console.log(i)
+    if(i == 1){
+        boardButton3.textContent = 'X'
+        i++
+    }else if(i == 2){
+        boardButton3.textContent = 'O'
+        i++
+    }else if(i == 3){
+        boardButton3.textContent = 'X'
+        i++
+    }else if(i == 4){
+        boardButton3.textContent = "O"
+        i++
+    }else if(i == 5){
+        boardButton3.textContent = "X"
+        i++
+    }else if(i == 6){
+        boardButton3.textContent = "O"
+        i++
+    }else if(i == 7){
+        boardButton3.textContent = "X"
+        i++
+    }else if(i == 8){
+        boardButton3.textContent = "O"
+        i++
+    }else if(i == 9){
+        boardButton3.textContent = 'X'
+    }
+})
+
+boardButton4.addEventListener('click', function(){
+    console.log(i)
+    if(i == 1){
+        boardButton4.textContent = 'X'
+        i++
+    }else if(i == 2){
+        boardButton4.textContent = 'O'
+        i++
+    }else if(i == 3){
+        boardButton4.textContent = 'X'
+        i++
+    }else if(i == 4){
+        boardButton4.textContent = "O"
+        i++
+    }else if(i == 5){
+        boardButton4.textContent = "X"
+        i++
+    }else if(i == 6){
+        boardButton4.textContent = "O"
+        i++
+    }else if(i == 7){
+        boardButton4.textContent = "X"
+        i++
+    }else if(i == 8){
+        boardButton4.textContent = "O"
+        i++
+    }else if(i == 9){
+        boardButton4.textContent = 'X'
+    }
+})
+
+boardButton5.addEventListener('click', function(){
+    console.log(i)
+    if(i == 1){
+        boardButton5.textContent = 'X'
+        i++
+    }else if(i == 2){
+        boardButton5.textContent = 'O'
+        i++
+    }else if(i == 3){
+        boardButton5.textContent = 'X'
+        i++
+    }else if(i == 4){
+        boardButton5.textContent = "O"
+        i++
+    }else if(i == 5){
+        boardButton5.textContent = "X"
+        i++
+    }else if(i == 6){
+        boardButton5.textContent = "O"
+        i++
+    }else if(i == 7){
+        boardButton5.textContent = "X"
+        i++
+    }else if(i == 8){
+        boardButton5.textContent = "O"
+        i++
+    }else if(i == 9){
+        boardButton5.textContent = 'X'
+    }
+})
+
+boardButton6.addEventListener('click', function(){
+    console.log(i)
+    if(i == 1){
+        boardButton6.textContent = 'X'
+        i++
+    }else if(i == 2){
+        boardButton6.textContent = 'O'
+        i++
+    }else if(i == 3){
+        boardButton6.textContent = 'X'
+        i++
+    }else if(i == 4){
+        boardButton6.textContent = "O"
+        i++
+    }else if(i == 5){
+        boardButton6.textContent = "X"
+        i++
+    }else if(i == 6){
+        boardButton6.textContent = "O"
+        i++
+    }else if(i == 7){
+        boardButton6.textContent = "X"
+        i++
+    }else if(i == 8){
+        boardButton6.textContent = "O"
+        i++
+    }else if(i == 9){
+        boardButton6.textContent = 'X'
+    }
+})
+
+boardButton7.addEventListener('click', function(){
+    console.log(i)
+    if(i == 1){
+        boardButton7.textContent = 'X'
+        i++
+    }else if(i == 2){
+        boardButton7.textContent = 'O'
+        i++
+    }else if(i == 3){
+        boardButton7.textContent = 'X'
+        i++
+    }else if(i == 4){
+        boardButton7.textContent = "O"
+        i++
+    }else if(i == 5){
+        boardButton7.textContent = "X"
+        i++
+    }else if(i == 6){
+        boardButton7.textContent = "O"
+        i++
+    }else if(i == 7){
+        boardButton7.textContent = "X"
+        i++
+    }else if(i == 8){
+        boardButton7.textContent = "O"
+        i++
+    }else if(i == 9){
+        boardButton7.textContent = 'X'
+    }
+})
+
+boardButton8.addEventListener('click', function(){
+    console.log(i)
+    if(i == 1){
+        boardButton8.textContent = 'X'
+        i++
+    }else if(i == 2){
+        boardButton8.textContent = 'O'
+        i++
+    }else if(i == 3){
+        boardButton8.textContent = 'X'
+        i++
+    }else if(i == 4){
+        boardButton8.textContent = "O"
+        i++
+    }else if(i == 5){
+        boardButton8.textContent = "X"
+        i++
+    }else if(i == 6){
+        boardButton8.textContent = "O"
+        i++
+    }else if(i == 7){
+        boardButton8.textContent = "X"
+        i++
+    }else if(i == 8){
+        boardButton8.textContent = "O"
+        i++
+    }else if(i == 9){
+        boardButton8.textContent = 'X'
+    }
+})
+
+boardButton9.addEventListener('click', function(){
+    console.log(i)
+    if(i == 1){
+        boardButton9.textContent = 'X'
+        i++
+    }else if(i == 2){
+        boardButton9.textContent = 'O'
+        i++
+    }else if(i == 3){
+        boardButton9.textContent = 'X'
+        i++
+    }else if(i == 4){
+        boardButton9.textContent = "O"
+        i++
+    }else if(i == 5){
+        boardButton9.textContent = "X"
+        i++
+    }else if(i == 6){
+        boardButton9.textContent = "O"
+        i++
+    }else if(i == 7){
+        boardButton9.textContent = "X"
+        i++
+    }else if(i == 8){
+        boardButton9.textContent = "O"
+        i++
+    }else if(i == 9){
+        boardButton9.textContent = 'X'
+    }
+})
+
+
 
 
